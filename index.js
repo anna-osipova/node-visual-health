4132b170d41b8484a9fe7ee3a6fcb90452901322
Anna Osipova <anna.osipova@cgi.com>
2017-05-19 08:12:58 +0300

    added mongoose

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -7,6 +9,18 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
     response.send("hello");
+});
+
+var uriString =
+    process.env.MONGODB_URI ||
+    'mongodb://localhost/HelloMongoose';
+
+mongoose.connect(uriString, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uriString + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uriString);
+    }
 });
 
 app.listen(app.get('port'), function() {
